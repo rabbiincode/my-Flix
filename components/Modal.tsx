cba95f5770c12b8e29cd60ec4bae10ab1e3e233e
RABBI <rabbiincode@gmail.com>
2022-08-01 07:58:26 -0700

    optional

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -62,7 +62,7 @@ const Modal = () => {
 
         <div className='relative pt-[56.25%]'>
           <ReactPlayer
-            url={`https://www.youtube.com/watch?v=${trailer}`}
+            url={`https://www.youtube.com/watch?v=${trailer} || "loading..." `}
             width="100%"
             height="100%"
             style={{ position: 'absolute', top: '0', left: '0' }}
